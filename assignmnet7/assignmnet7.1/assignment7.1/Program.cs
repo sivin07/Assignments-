83b1bd9cc6e3b477558affc5f4d4756df83b7af5
SIVIN WILLS <sivinwillssivin@gmail.com>
2026-01-03 00:49:20 +0530

    Added Assignment 7

---
+++ assignmnet7/assignmnet7.1/assignment7.1/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace assignment7._1
+{
+    internal class Program
+    {
+
+        class Person
+        {
+            internal string name;
+            internal int age;
+
+        public void Readdata()
+            {
+                Console.WriteLine("Enter the  name");
+                name=Console.ReadLine();
+
+                Console.WriteLine("Enter age");
+                age=int.Parse(Console.ReadLine());  
+
+
+            }
+
+            public void DisplayData()
+            {
+                Console.WriteLine($"the name of  person:{name}");
+               
+                Console.WriteLine($"the age of the person:{age}");
+
+            }
+
+            public override string ToString()
+            {
+                return "\nName of person" + name +
+                        "age of the person" + age;
+            }
+        }
+
+
+        //student class 
+        class Student : Person
+        {
+            internal int roll;
+            internal int marks;
+            internal string grade;
+
+            public void GetData()
+            {
+                Console.WriteLine("Enter roll number:");
+                roll = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter marks:");
+                marks = int.Parse(Console.ReadLine());
+            }
+
+            public void ComputeGrade()
+            {
+                if (marks >= 90)
+                    grade = "A";
+                else if (marks >= 75)
+                    grade = "B";
+                else if (marks >= 60)
+                    grade = "C";
+                else
+                    grade = "Fail";
+            }
+
+            public void ShowData()
+            {
+                DisplayData();   // from Person
+                Console.WriteLine($"Roll Number: {roll}");
+                Console.WriteLine($"Marks: {marks}");
+                Console.WriteLine($"Grade: {grade}");
+            }
+        }
+        //employee data 
+        class Employee : Person
+        {
+            internal double bp;
+            internal double hr;
+            internal double salary;
+
+            public void GetData()
+            {
+                Console.WriteLine("Enter Basic Pay:");
+                bp = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter HRA:");
+                hr = double.Parse(Console.ReadLine());
+            }
+
+            public void ComputeSalary()
+            {
+                salary = bp + hr;
+            }
+
+            public void ShowData()
+            {
+                DisplayData();   // from Person
+                Console.WriteLine($"Basic Pay: {bp}");
+                Console.WriteLine($"HRA: {hr}");
+                Console.WriteLine($"Salary: {salary}");
+            }
+        }
+
+
+
+
+       
+            static void Main(string[] args)
+            {
+                Console.WriteLine("STUDENT DETAILS");
+                Student student1 = new Student();
+                student1.Readdata();      // Person method
+                student1.GetData();       // Student method
+                student1.ComputeGrade();
+                student1.ShowData();
+
+                Console.WriteLine("\nEMPLOYEE DETAILS");
+                Employee employee1 = new Employee();
+                employee1.Readdata();      // Person method
+                employee1.GetData();       // Employee method
+                employee1.ComputeSalary();
+                employee1.ShowData();
+
+                Console.ReadKey();
+            }
+
+
+        }
+    }
---
+++ assignmnet7/assignmnet7.1/assignment7.2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Reflection.Metadata;
+
+namespace assignment7._2
+{
+    internal class Program
+    {
+        class Employee
+        {
+            internal string name;
+            internal string phoneNumber;
+            internal string designation;
+            internal float basicPay;
+
+            // Constructor
+            public Employee(string _names, string _phoneNumbers, string _designations, float _basicPays)
+            {
+                name = _names;
+                phoneNumber = _phoneNumbers;
+                designation = _designations;
+                basicPay = _basicPays;
+            }
+
+            public virtual void Display()
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Phone Number: " + phoneNumber);
+                Console.WriteLine("Designation: " + designation);
+                Console.WriteLine("Basic Pay: " + basicPay);
+            }
+        }
+
+        // Derived class
+        class Executive : Employee
+        {
+            public Executive(string _names, string _phoneNumbers, string _designations, float _basicPays)
+                : base(_names, _phoneNumbers, _designations, _basicPays)
+            {
+            }
+
+            public override void Display()
+            {
+                if (basicPay > 10000)
+                {
+                    Console.WriteLine("Executive Details:");
+                    base.Display();
+                }
+                else
+                {
+                    Console.WriteLine("Employee is NOT eligible to be an Executive (Basic Pay ≤ 10000)");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter phone number:");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("Enter designation:");
+            string designation = Console.ReadLine();
+
+            Console.WriteLine("Enter basic pay:");
+            float basicPay = float.Parse(Console.ReadLine());
+
+            Executive exec = new Executive(name, phone, designation, basicPay);
+            exec.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
